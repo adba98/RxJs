38b1f6ccf64f2591a282ecfcaac5c71f2a744215
adba98 <oscar.adba@gmail.com>
2022-04-23 20:38:51 -0500

    Filter operator chain

--- src/operators/03-filter.ts
+++ src/operators/03-filter.ts
@@ -1,5 +1,5 @@
-import { from } from 'rxjs';
-import { filter } from 'rxjs/operators';
+import { from, fromEvent } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 
 interface Character {
   type: string;
@@ -27,3 +27,10 @@ range(5, 15)
 from(character)
   .pipe(filter((val) => val.type === 'villano'))
   .subscribe(console.log);
+
+const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup').pipe(
+  map((event) => event.code),
+  filter((key) => key === 'Enter'),
+  filter((key) => key === 'Enter')
+);
+keyup$.subscribe(console.log);
